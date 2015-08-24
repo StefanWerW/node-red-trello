7625a8cd9742811321401fb9a69a6a6d29744d16
StefanW <scaw.dev@gmail.com>
2015-08-24 11:05:57 +0200

    fix

--- node-red-trello.js
+++ node-red-trello.js
@@ -1,17 +1,34 @@
 module.exports = function(RED){
 
-  
+  //CREDENTIALS
+  function TrelloNode(n) {
+        RED.nodes.createNode(this,n);
+        this.user_me = n.user_me;
+    }
+    RED.nodes.registerType("trello-credentials",TrelloNode,{
+        credentials: {
+            user_me: {type:"text"},
+            access_token: {type: "password"},
+            access_token_secret: {type:"password"}
+        }
+    });
 
-
+  //CREATE CARD
   function CreateCard(config){
     RED.nodes.createNode(this,config);
-      var node = this;
-      this.on('input', function(msg) {
+    var node = this;
+    this.on('input', function(msg) {
 
     });
 
   }
   RED.nodes.registerType("create-card",CreateCard);
 
+  //GET BOARDS
+  function GetBoards(config){
+    RED.nodes.createNode(this, config);
+
+  }
+  RED.nodes.registerType("get-boards", GetBoards);
 
 }
